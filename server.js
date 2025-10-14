57b333ff92b4a5651f61e3f53372cdf5e7dc5e75
Guillermo-Gonzalez-cd <domfenix215@gmail.com>
2025-10-14 15:56:23 -0600

    Cambio de puerto a 4001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
@@ -9,20 +9,26 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
+// Ruta simple para comprobar el estado del servidor
 app.get("/status", (req, res) => {
     res.json({ msg: "Servidor TicTacToe Corriendo" });
 });
 
+// Evento de conexión de Socket.IO
 io.on("connection", (socket) => {
     console.log("Jugador Conectado: ", socket.id);
+
+    // Enviar mensaje de bienvenida al cliente
     socket.emit("welcome", { msg: "Bienvenido a TicTacToe" });
+
+    // Escuchar jugadas del cliente
+    socket.on("move", (data) => {
+        console.log("Movimiento recibido:", data);
+        io.emit("move", data); // reenviar jugada a todos los clientes
+    });
 });
 
-server.listen(3000, () => {
-    console.log("Servidor escuchando en http://localhost:3000");
-});
-
-socket.on("move", (data) => {
-  console.log("Movimiento recibido:", data);
-  io.emit("move", data); // enviar la jugada a todos los clientes conectados
+// Escuchar en el puerto 4001
+server.listen(4001, () => {
+    console.log("Servidor escuchando en http://localhost:4001");
 });
